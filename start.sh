e92534c53d63183b2b6e25a404dc564125decb07
ripps818 <ripps818@gmail.com>
2024-02-15 07:20:27 -0600

    implement UPDATE_ON_BOOT and use printf
    initial UPDATE_ON_BOOT flag for start.sh, also switched some echo promts for printf so that they are more visible

--- start.sh
+++ start.sh
@@ -4,23 +4,16 @@ export WINEDEBUG=-all
 steamcmd_url="https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
 
 if [ ! -d "$WINEPREFIX" ]; then
-  echo ""
-  echo "Initializing Wine configuration"
-  echo ""
+  printf "\e[0;32m%s\e[0m\n" "Initializing Wine configuration"
   wineboot --init && wineserver -w
 fi
 
 if [ ! -f /app/steamcmd.exe ]; then
-  echo ""
-  echo "Downloading SteamCmd for Windows"
-  echo ""
+  printf "\e[0;32m%s\e[0m\n" "Downloading SteamCmd for Windows"
   wget -O /app/steamcmd.zip ${steamcmd_url}
   unzip /app/steamcmd.zip*
   rm -rf /app/steamcmd.zip*
-  echo ""
-  echo "Installing Palworld Server..."
-  echo "Container might need to be restarted when done"
-  echo ""
+  printf "\e[0;32m%s\e[0m\n" "Installing Palworld Server...\nContainer might need to be restarted when done"
 fi
 
 # Install Visual C++ Runtime
@@ -28,19 +21,16 @@ fi
 --optout -f -q vcrun2022 && \
 wineserver -w
 
-# Install/Update Palworld Server
-echo ""
-echo "Updating and Validating Palworld Server"
-echo ""
-/usr/bin/wine \
-/app/steamcmd.exe \
-+login anonymous +app_update 2394010 validate +quit && \
-wineserver -w
-
-# Start Palworld Server
-echo ""
-echo "Starting Palword Server"
-echo ""
+# Update Palworld Server
+if [ "${UPDATE_ON_BOOT,,}" = true ]; then
+	printf "\e[0;32m%s\e[0m\n" "Updating and Validating Palworld Server"
+	/usr/bin/wine \
+	/app/steamcmd.exe \
+	+login anonymous +app_update 2394010 validate +quit && \
+	wineserver -w
+fi
+	# Start Palworld Server
+	printf "\e[0;32m%s\e[0m\n" "Starting Palword Server"
 /usr/bin/wine \
 /app/steamapps/common/PalServer/Pal/Binaries/Win64/PalServer-Win64-Test-Cmd.exe \
 -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS
