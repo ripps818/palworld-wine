b5531bac61b69e7abcdd594111dda10d2e1d2bb3
ripps818 <ripps818@gmail.com>
2024-02-15 11:06:36 -0600

    cleanup Xvfb so it starts and shutsdown more cleanly

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 chown -R $PUID:$PGID /app /backup
 
-Xvfb :99 -screen 0 640x480x8 -nolisten tcp &
+# make sure Xvfb is dead
+rm /tmp/.X*-lock
+Xvfb $DISPLAY -screen 0 640x480x8 -nolisten tcp &
+XVFB_PROC=$!
 
 /gosu $PUID:$PGID /start.sh
+
+kill $XVFB_PROC
