e3738f9252d10d53044518f8728b99efbb568003
ripps818 <ripps818@gmail.com>
2024-02-15 07:22:01 -0600

    minor indentation update

--- start.sh
+++ start.sh
@@ -4,16 +4,16 @@ export WINEDEBUG=-all
 steamcmd_url="https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
 
 if [ ! -d "$WINEPREFIX" ]; then
-  printf "\e[0;32m%s\e[0m\n" "Initializing Wine configuration"
-  wineboot --init && wineserver -w
+	printf "\e[0;32m%s\e[0m\n" "Initializing Wine configuration"
+	wineboot --init && wineserver -w
 fi
 
 if [ ! -f /app/steamcmd.exe ]; then
-  printf "\e[0;32m%s\e[0m\n" "Downloading SteamCmd for Windows"
-  wget -O /app/steamcmd.zip ${steamcmd_url}
-  unzip /app/steamcmd.zip*
-  rm -rf /app/steamcmd.zip*
-  printf "\e[0;32m%s\e[0m\n" "Installing Palworld Server...\nContainer might need to be restarted when done"
+	printf "\e[0;32m%s\e[0m\n" "Downloading SteamCmd for Windows"
+	wget -O /app/steamcmd.zip ${steamcmd_url}
+	unzip /app/steamcmd.zip*
+	rm -rf /app/steamcmd.zip*
+	printf "\e[0;32m%s\e[0m\n" "Installing Palworld Server...\nContainer might need to be restarted when done"
 fi
 
 # Install Visual C++ Runtime
@@ -29,8 +29,9 @@ if [ "${UPDATE_ON_BOOT,,}" = true ]; then
 	+login anonymous +app_update 2394010 validate +quit && \
 	wineserver -w
 fi
-	# Start Palworld Server
-	printf "\e[0;32m%s\e[0m\n" "Starting Palword Server"
+
+# Start Palworld Server
+printf "\e[0;32m%s\e[0m\n" "Starting Palword Server"
 /usr/bin/wine \
 /app/steamapps/common/PalServer/Pal/Binaries/Win64/PalServer-Win64-Test-Cmd.exe \
 -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS
